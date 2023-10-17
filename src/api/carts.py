b16a9361530070551a299f6c14e3710bdf6c37b6
n-shinde <nidhishinde01@gmail.com>
2023-10-16 22:42:56 -0700

    Version 3.4

--- src/api/carts.py
+++ src/api/carts.py
@@ -22,12 +22,12 @@ def create_cart(new_cart: NewCart):
     global cart_id 
     cart_id += 1
 
-    print("cart_id:" + cart_id)
+    print(cart_id)
 
     name = new_cart.customer
     customer_id = cart_id
 
-    print("customer_id" + customer_id)
+    print(customer_id)
 
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("INSERT INTO carts (cart_id, customer_name) VALUES (:customer_id, :name)"))
